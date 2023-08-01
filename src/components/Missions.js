352568bc04fa0f44162f8d421ee80a6cd879763c
DevaWinner <winneraaniekan@gmail.com>
2023-08-01 13:39:42 +0100

    Add td of status with dummy text of "upcoming"

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -27,6 +27,7 @@ const MissionTable = () => {
           <th>Mission</th>
           <th>Description</th>
           <th>Status</th>
+          <th> </th>
         </tr>
       </thead>
       <tbody>
@@ -36,6 +37,7 @@ const MissionTable = () => {
               <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
               <td>Upcoming</td>
+              <td><button type="button">Join Mission</button></td>
             </tr>
           ))}
       </tbody>
