abb5132df845f0a09e6f8408f2f7d82d62854f0a
DianaBeki <dianabeki54@gmail.com>
2023-08-01 17:56:24 +0300

    display rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRocketsData } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <div>
-    <h2>Rockets</h2>
-  </div>
-);
+const Rockets = () => {
+  const rocketsData = useSelector((state) => state.rocketsList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRocketsData());
+  }, [dispatch]);
+
+  if (rocketsData.loading) return <p className="fetchStatus">Loading...</p>;
+  if (rocketsData.errors) return <p className="error">Error loading the rockets</p>;
+  return (
+    <ul className="rocketsList">
+      {rocketsData.value.map((e, i) => (
+        <>
+          key=
+          {e.id}
+          id=
+          {e.id}
+          indexNo=
+          {i}
+          title=
+          {e.name}
+          description=
+          {e.description}
+          img=
+          {e.flickrImages[0]}
+          reserved=
+          {e.reserved}
+        </>
+      ))}
+    </ul>
+  );
+};
 
 export default Rockets;
