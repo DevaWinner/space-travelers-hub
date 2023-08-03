c03ef0ffceed8b6500e3e9d373cfb4d59cbe0835
DevaWinner <winneraaniekan@gmail.com>
2023-08-03 18:54:31 +0100

    Style my profiles

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -14,29 +14,29 @@ const Profile = () => {
   const myJoinedMissions = missions.filter((mission) => joinedMissions.includes(mission.mission_id));
   return (
     <section className="profile-container">
-      <div className="column">
-        <h2 className="column-head">My Missions</h2>
+      <div className="mission-column">
+        <h2>My Missions</h2>
         {myJoinedMissions.length === 0 ? (
           <p className="empty-message">No joined missions yet.</p>
         ) : (
           <ul className="u-list">
             {myJoinedMissions.map((mission) => (
-              <li className="profile-list" key={mission.mission_id}>
-                <h4 className="my-missions-name">{mission.mission_name}</h4>
+              <li key={mission.mission_id}>
+                <p>{mission.mission_name}</p>
               </li>
             ))}
           </ul>
         )}
       </div>
-      <div className="column">
-        <h2 className="column-head">My Rockets</h2>
+      <div className="rocket-column">
+        <h2>My Rockets</h2>
         {myReservedRockets.length === 0 ? (
           <p className="empty-message">No reserved rockets yet.</p>
         ) : (
           <ul className="u-list">
             {myReservedRockets.map((reservations) => (
-              <li className="profile-list" key={reservations.id}>
-                <h4 className="my-missions-name">{reservations.rocket_name}</h4>
+              <li key={reservations.id}>
+                <p>{reservations.rocket_name}</p>
               </li>
             ))}
           </ul>
