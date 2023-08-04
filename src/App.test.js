48decec76e018ed0a72be731ed68daa435b9d9b9
DevaWinner <winneraaniekan@gmail.com>
2023-08-04 10:35:55 +0100

    Move jest-dom to dependency

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
