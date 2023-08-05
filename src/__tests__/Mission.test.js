dc05667c7ebe5dc1b72b605aac5258dfb3f882a8
DevaWinner <winneraaniekan@gmail.com>
2023-08-05 20:53:26 +0100

    Test: Renders correctly

--- src/__tests__/Mission.test.js
+++ src/__tests__/Mission.test.js
@@ -47,4 +47,14 @@ describe('Mission functions', () => {
 
     expect(handleLeaveMissionMock).toHaveBeenCalledWith(dummyMission.mission_id);
   });
+
+  it('renders correctly', () => {
+    const { getByText } = renderWithProvider(
+      <Mission mission={dummyMission} handleJoinMission={() => {}} handleLeaveMission={() => {}} />,
+    );
+
+    expect(getByText('Test Mission')).toBeInTheDocument();
+    expect(getByText('This is a test mission.')).toBeInTheDocument();
+    expect(getByText('Join Mission')).toBeInTheDocument();
+  });
 });
