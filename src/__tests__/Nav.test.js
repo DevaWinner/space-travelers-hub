bdd5d6bb313e36c3cb88c3f4e531e19a5ea1f2ad
DevaWinner <winneraaniekan@gmail.com>
2023-08-05 20:50:22 +0100

    Update Nav test

--- src/__tests__/Nav.test.js
+++ src/__tests__/Nav.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import Nav from './Nav';
+import Nav from '../components/Nav';
 
 test('renders Nav component', () => {
   render(
